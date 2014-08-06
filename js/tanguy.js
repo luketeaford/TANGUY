15b2048816a53e55581bc5281a59f665477cf6ad
Luke Teaford <luketeaford@gmail.com>
2014-08-06 01:23:39 -0400

    Fix envelope infinite gain problem
    Prevents keyup from happening if the user holds down c, plays another
    note, and then lets go of the second note.

--- js/tanguy.js
+++ js/tanguy.js
@@ -1043,6 +1043,7 @@ $(document).keypress(function (key) {
     };
 }).keydown(function (key) {
     if (TANGUY.key_down == false) {
+        TANGUY.key_active = key.which;
         TANGUY.key_down = true;
         switch (key.which) {
             case 65:
@@ -1105,64 +1106,66 @@ $(document).keypress(function (key) {
         };
     };
 }).keyup(function (key) {
-    TANGUY.key_down = false;
-    switch (key.which) {
-        case 65:
-            $('#c1').trigger('mouseup').removeClass('playing');
-            break;
-        case 83:
-            $('#d1').trigger('mouseup').removeClass('playing');
-            break;
-        case 68:
-            $('#e1').trigger('mouseup').removeClass('playing');
-            break;
-        case 70:
-            $('#f1').trigger('mouseup').removeClass('playing');
-            break;
-        case 71:
-            $('#g1').trigger('mouseup').removeClass('playing');
-            break;
-        case 72:
-            $('#a1').trigger('mouseup').removeClass('playing');
-            break;
-        case 74:
-            $('#b1').trigger('mouseup').removeClass('playing');
-            break;
-        case 75:
-            $('#c2').trigger('mouseup').removeClass('playing');
-            break;
-        case 76:
-            $('#d2').trigger('mouseup').removeClass('playing');
-            break;
-        case 186:
-            $('#e2').trigger('mouseup').removeClass('playing');
-            break;
-        case 222:
-            $('#f2').trigger('mouseup').removeClass('playing');
-            break;
-        case 87:
-            $('#cs1').trigger('mouseup').removeClass('playing');
-            break;
-        case 69:
-            $('#ds1').trigger('mouseup').removeClass('playing');
-            break;
-        case 84:
-            $('#fs1').trigger('mouseup').removeClass('playing');
-            break;
-        case 89:
-            $('#gs1').trigger('mouseup').removeClass('playing');
-            break;
-        case 85:
-            $('#as1').trigger('mouseup').removeClass('playing');
-            break;
-        case 79:
-            $('#cs2').trigger('mouseup').removeClass('playing');
-            break;
-        case 80:
-            $('#ds2').trigger('mouseup').removeClass('playing');
-            break;
-        case 221:
-            $('#fs2').trigger('mouseup').removeClass('playing');
-            break;
+    if (key.which === TANGUY.key_active) {
+        TANGUY.key_down = false;
+        switch (key.which) {
+            case 65:
+                $('#c1').trigger('mouseup').removeClass('playing');
+                break;
+            case 83:
+                $('#d1').trigger('mouseup').removeClass('playing');
+                break;
+            case 68:
+                $('#e1').trigger('mouseup').removeClass('playing');
+                break;
+            case 70:
+                $('#f1').trigger('mouseup').removeClass('playing');
+                break;
+            case 71:
+                $('#g1').trigger('mouseup').removeClass('playing');
+                break;
+            case 72:
+                $('#a1').trigger('mouseup').removeClass('playing');
+                break;
+            case 74:
+                $('#b1').trigger('mouseup').removeClass('playing');
+                break;
+            case 75:
+                $('#c2').trigger('mouseup').removeClass('playing');
+                break;
+            case 76:
+                $('#d2').trigger('mouseup').removeClass('playing');
+                break;
+            case 186:
+                $('#e2').trigger('mouseup').removeClass('playing');
+                break;
+            case 222:
+                $('#f2').trigger('mouseup').removeClass('playing');
+                break;
+            case 87:
+                $('#cs1').trigger('mouseup').removeClass('playing');
+                break;
+            case 69:
+                $('#ds1').trigger('mouseup').removeClass('playing');
+                break;
+            case 84:
+                $('#fs1').trigger('mouseup').removeClass('playing');
+                break;
+            case 89:
+                $('#gs1').trigger('mouseup').removeClass('playing');
+                break;
+            case 85:
+                $('#as1').trigger('mouseup').removeClass('playing');
+                break;
+            case 79:
+                $('#cs2').trigger('mouseup').removeClass('playing');
+                break;
+            case 80:
+                $('#ds2').trigger('mouseup').removeClass('playing');
+                break;
+            case 221:
+                $('#fs2').trigger('mouseup').removeClass('playing');
+                break;
+        };
     };
 });
