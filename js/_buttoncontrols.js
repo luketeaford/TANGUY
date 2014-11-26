1a21e96ca7c3dba0fd448304028d2e35989d569a
Luke Teaford <luketeaford@gmail.com>
2014-11-25 22:39:46 -0500

    Work in progress multi-switch
    Still needs keyframe animation. There must be a better way to do the
    Sass part of it. Multiswitch is getting cumbersome with something that
    should be another function...

--- js/_buttoncontrols.js
+++ js/_buttoncontrols.js
@@ -10,7 +10,30 @@ TANGUY.button = {
     },
     change: function (e) {
         'use strict';
-        var button = e.currentTarget === undefined ? $(e) : $(e.currentTarget);
+        var button = e.currentTarget === undefined ? $(e) : $(e.currentTarget),
+            bg_pos;
+        switch (button.val()) {
+        case '0.5':
+            console.log('I knew it!');
+            bg_pos = 'pos1';
+            break;
+        case '1':
+            console.log('Sweet');
+            bg_pos = 'pos2';
+            break;
+        case '2':
+            console.log('Two to tango');
+            bg_pos = 'pos3';
+            break;
+        case '4':
+            console.log('Eyes');
+            bg_pos = 'pos4';
+            break;
+        default:
+            console.log('DANGER! NOTHING!');
+            break;
+        }
+        button.parent().parent().removeClass().addClass(bg_pos);//remove all classes then add the proper one
         return button.parent().addClass('selected').siblings().removeClass('selected');
     }
 };
