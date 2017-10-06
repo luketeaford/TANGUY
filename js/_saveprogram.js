bac07ee76fe33ce55e76d3cf31d77fe792188f26
Luke Teaford <luketeaford@gmail.com>
2017-10-06 17:29:07 -0400

    Get a fresh start

--- js/_saveprogram.js
+++
@@ -1,6 +0,0 @@
-TANGUY.save_program = function () {
-    'use strict';
-    var patch_name = prompt('PATCH NAME');
-    TANGUY.program.name = patch_name;
-    console.log('SAVE PROGRAM: ' + JSON.stringify(TANGUY.program));
-};
